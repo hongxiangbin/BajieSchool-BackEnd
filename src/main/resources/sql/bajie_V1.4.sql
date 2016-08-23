195d020abab8ed17165b4fd39b13d3c9135707a0
EclipseXuLu <249048056@qq.com>
2016-08-23 15:26:02 +0800

    ——V0.2.1.20160823_alpha ——动态查询学习列表l

--- src/main/resources/sql/bajie_V1.4.sql
+++ src/main/resources/sql/bajie_V1.4.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50520
+Source Server         : BajieSchool
+Source Server Version : 50632
 Source Host           : localhost:3306
-Source Database       : bajieschool
+Source Database       : bajie
 
 Target Server Type    : MYSQL
-Target Server Version : 50520
+Target Server Version : 50632
 File Encoding         : 65001
 
-Date: 2016-08-19 20:33:26
+Date: 2016-08-23 15:24:43
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -40,12 +40,15 @@ CREATE TABLE `activity` (
 -- ----------------------------
 -- Records of activity
 -- ----------------------------
+INSERT INTO `activity` VALUES ('10000', '0', '周杰伦演唱会', '8月17日，周杰伦将在武汉光谷广场举办演唱会', 'img/activity/act/周杰伦.jpg', '2016-08-16 14:49:44', '520', '2465', '249', '125', '154', '湖北武汉', null, null);
+INSERT INTO `activity` VALUES ('10001', '0', '科技展览', 'VR，无人机，无人驾驶，机器人等在武汉大学开办展会', 'img/activity/act/科技展览.jpeg', '2016-08-15 14:49:44', '52', '255', '199', '125', '174', '湖北武汉', null, null);
+INSERT INTO `activity` VALUES ('10002', '0', '数学建模大赛', '阿里巴巴天池大数据竞赛', 'img/activity/act/阿里巴巴.jpg', '2016-08-14 14:49:44', '52', '255', '1699', '125', '174', '浙江杭州', null, null);
 
 -- ----------------------------
--- Table structure for activity-comment
+-- Table structure for activity_comment
 -- ----------------------------
-DROP TABLE IF EXISTS `activity-comment`;
-CREATE TABLE `activity-comment` (
+DROP TABLE IF EXISTS `activity_comment`;
+CREATE TABLE `activity_comment` (
   `act_com_id` varchar(12) NOT NULL,
   `username` varchar(16) NOT NULL,
   `act_com_content` varchar(255) DEFAULT NULL,
@@ -55,14 +58,14 @@ CREATE TABLE `activity-comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of activity-comment
+-- Records of activity_comment
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for activity-comment-comment
+-- Table structure for activity_comment-comment
 -- ----------------------------
-DROP TABLE IF EXISTS `activity-comment-comment`;
-CREATE TABLE `activity-comment-comment` (
+DROP TABLE IF EXISTS `activity_comment-comment`;
+CREATE TABLE `activity_comment-comment` (
   `a_c_c_id` varchar(12) NOT NULL COMMENT '评论ID',
   `act_com_id` varchar(12) NOT NULL COMMENT '原评论ID',
   `username` varchar(16) NOT NULL COMMENT '评论用户',
@@ -72,22 +75,30 @@ CREATE TABLE `activity-comment-comment` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of activity-comment-comment
+-- Records of activity_comment-comment
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for activity-type
+-- Table structure for activity_type
 -- ----------------------------
-DROP TABLE IF EXISTS `activity-type`;
-CREATE TABLE `activity-type` (
-  `act_type_id` varchar(2) NOT NULL,
+DROP TABLE IF EXISTS `activity_type`;
+CREATE TABLE `activity_type` (
+  `act_type_id` int(2) NOT NULL,
   `act_type_name` varchar(20) NOT NULL,
   PRIMARY KEY (`act_type_id`,`act_type_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of activity-type
+-- Records of activity_type
 -- ----------------------------
+INSERT INTO `activity_type` VALUES ('0', '推荐');
+INSERT INTO `activity_type` VALUES ('1', '旅游');
+INSERT INTO `activity_type` VALUES ('2', '高校');
+INSERT INTO `activity_type` VALUES ('3', '明星');
+INSERT INTO `activity_type` VALUES ('4', '电影');
+INSERT INTO `activity_type` VALUES ('5', '音乐');
+INSERT INTO `activity_type` VALUES ('6', '体育');
+INSERT INTO `activity_type` VALUES ('7', '恋爱');
 
 -- ----------------------------
 -- Table structure for agenda
@@ -141,18 +152,21 @@ CREATE TABLE `collection` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for login-status
+-- Table structure for login_status
 -- ----------------------------
-DROP TABLE IF EXISTS `login-status`;
-CREATE TABLE `login-status` (
+DROP TABLE IF EXISTS `login_status`;
+CREATE TABLE `login_status` (
   `username` varchar(16) NOT NULL,
-  `status` char(1) NOT NULL,
+  `status` int(1) NOT NULL,
+  `logintime` datetime DEFAULT NULL,
   PRIMARY KEY (`username`,`status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of login-status
+-- Records of login_status
 -- ----------------------------
+INSERT INTO `login_status` VALUES ('admin', '0', '2016-08-23 13:44:10');
+INSERT INTO `login_status` VALUES ('LucasX', '0', '2016-08-24 13:44:10');
 
 -- ----------------------------
 -- Table structure for notification
@@ -177,6 +191,7 @@ DROP TABLE IF EXISTS `question`;
 CREATE TABLE `question` (
   `que_id` varchar(12) NOT NULL COMMENT '问题ID',
   `que_title` varchar(100) NOT NULL COMMENT '问题标题',
+  `que_tags` varchar(50) DEFAULT NULL COMMENT '问题标签',
   `que_content` varchar(255) DEFAULT NULL COMMENT '问题内容',
   `que_img` varchar(100) DEFAULT NULL COMMENT '图片',
   `que_like` int(5) DEFAULT NULL COMMENT '赞同数',
@@ -242,10 +257,10 @@ CREATE TABLE `study` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for study-reply
+-- Table structure for study_reply
 -- ----------------------------
-DROP TABLE IF EXISTS `study-reply`;
-CREATE TABLE `study-reply` (
+DROP TABLE IF EXISTS `study_reply`;
+CREATE TABLE `study_reply` (
   `std_id` varchar(12) NOT NULL COMMENT '学习ID',
   `username` varchar(16) NOT NULL COMMENT '用户名',
   `std_re_content` varchar(255) DEFAULT NULL COMMENT '回复内容',
@@ -255,22 +270,34 @@ CREATE TABLE `study-reply` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of study-reply
+-- Records of study_reply
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for study-type
+-- Table structure for study_type
 -- ----------------------------
-DROP TABLE IF EXISTS `study-type`;
-CREATE TABLE `study-type` (
+DROP TABLE IF EXISTS `study_type`;
+CREATE TABLE `study_type` (
   `type_id` varchar(12) NOT NULL,
   `type_name` varchar(20) NOT NULL,
   PRIMARY KEY (`type_id`,`type_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of study-type
+-- Records of study_type
 -- ----------------------------
+INSERT INTO `study_type` VALUES ('50000', '推荐');
+INSERT INTO `study_type` VALUES ('50001', '考研');
+INSERT INTO `study_type` VALUES ('50002', '英语');
+INSERT INTO `study_type` VALUES ('50003', '高数');
+INSERT INTO `study_type` VALUES ('50004', '计算机二级');
+INSERT INTO `study_type` VALUES ('50005', '财会');
+INSERT INTO `study_type` VALUES ('50006', '生科');
+INSERT INTO `study_type` VALUES ('50007', '机械');
+INSERT INTO `study_type` VALUES ('50008', '土建');
+INSERT INTO `study_type` VALUES ('50009', '经管');
+INSERT INTO `study_type` VALUES ('50010', '考公');
+INSERT INTO `study_type` VALUES ('50011', '四六级');
 
 -- ----------------------------
 -- Table structure for test_user
@@ -281,13 +308,12 @@ CREATE TABLE `test_user` (
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of test_user
 -- ----------------------------
-INSERT INTO `test_user` VALUES ('1', 'dsa', 'sda');
-INSERT INTO `test_user` VALUES ('2', 'd2ws', 'dsad');
+INSERT INTO `test_user` VALUES ('1', 'ewqe', 'ewq');
 
 -- ----------------------------
 -- Table structure for university
@@ -302,6 +328,8 @@ CREATE TABLE `university` (
 -- ----------------------------
 -- Records of university
 -- ----------------------------
+INSERT INTO `university` VALUES ('10486', '武汉大学');
+INSERT INTO `university` VALUES ('10500', '湖北工业大学');
 
 -- ----------------------------
 -- Table structure for user
@@ -331,12 +359,14 @@ CREATE TABLE `user` (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('admin', '1110831116', 'admin', '1', '13207145966', null, '10486', '信息管理学院', '2011', '1', '249048056', 'xulu0620', 'xldev', null, null, null, null);
+INSERT INTO `user` VALUES ('LucasX', '1234567894', '123456', '1', '13207145966', null, '10486', '经济管理学院', '2016', '1', '249048056', 'xulu0620', 'xldev', null, null, null, null);
 
 -- ----------------------------
--- Table structure for user-activity
+-- Table structure for user_activity
 -- ----------------------------
-DROP TABLE IF EXISTS `user-activity`;
-CREATE TABLE `user-activity` (
+DROP TABLE IF EXISTS `user_activity`;
+CREATE TABLE `user_activity` (
   `username` varchar(16) NOT NULL COMMENT '用户名',
   `act_id` varchar(12) NOT NULL COMMENT '活动ID',
   `flag` char(1) DEFAULT NULL COMMENT '0——关注的活动；1——收藏的活动；2——参加的活动；3——报名的活动',
@@ -344,14 +374,17 @@ CREATE TABLE `user-activity` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user-activity
+-- Records of user_activity
 -- ----------------------------
+INSERT INTO `user_activity` VALUES ('admin', '10000', '0');
+INSERT INTO `user_activity` VALUES ('admin', '10001', '0');
+INSERT INTO `user_activity` VALUES ('LucasX', '10002', '0');
 
 -- ----------------------------
--- Table structure for user-study
+-- Table structure for user_study
 -- ----------------------------
-DROP TABLE IF EXISTS `user-study`;
-CREATE TABLE `user-study` (
+DROP TABLE IF EXISTS `user_study`;
+CREATE TABLE `user_study` (
   `username` varchar(16) NOT NULL,
   `std_id` varchar(12) NOT NULL,
   `flag` char(1) DEFAULT NULL,
@@ -359,8 +392,29 @@ CREATE TABLE `user-study` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of user-study
+-- Records of user_study
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for user_studytype
+-- ----------------------------
+DROP TABLE IF EXISTS `user_studytype`;
+CREATE TABLE `user_studytype` (
+  `username` varchar(16) NOT NULL,
+  `stu_type` varchar(12) NOT NULL,
+  PRIMARY KEY (`username`,`stu_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user_studytype
+-- ----------------------------
+INSERT INTO `user_studytype` VALUES ('admin', '50000');
+INSERT INTO `user_studytype` VALUES ('admin', '50001');
+INSERT INTO `user_studytype` VALUES ('admin', '50002');
+INSERT INTO `user_studytype` VALUES ('admin', '50003');
+INSERT INTO `user_studytype` VALUES ('admin', '50004');
+INSERT INTO `user_studytype` VALUES ('admin', '50008');
+INSERT INTO `user_studytype` VALUES ('LucasX', '50011');
 
 -- ----------------------------
 -- Table structure for visitor
@@ -375,3 +429,9 @@ CREATE TABLE `visitor` (
 -- ----------------------------
 -- Records of visitor
 -- ----------------------------
+
+-- ----------------------------
+-- View structure for user_activity_view
+-- ----------------------------
+DROP VIEW IF EXISTS `user_activity_view`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `user_activity_view` AS select `act`.`act_id` AS `act_id`,`act`.`act_title` AS `act_title`,`act`.`act_content` AS `act_content`,`act`.`act_img` AS `act_img`,`act`.`act_time` AS `act_time`,`act`.`act_like` AS `act_like`,`act`.`act_comment` AS `act_comment` from ((`activity` `act` join `user_activity`) join `login_status`) where ((`user_activity`.`act_id` = `act`.`act_id`) and (`user_activity`.`username` = `login_status`.`username`) and (`login_status`.`status` = 0)) ;
